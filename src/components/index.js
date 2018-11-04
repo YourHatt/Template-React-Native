5c774a23c25721e652715292f9c1807247051505
unknown <you@example.comgraveinsabre@gmail.com>
2018-11-05 00:20:22 +0700

    clearn

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as HeaderMenu } from './HeaderMenu/HeaderMenu';
-export { default as ShowList } from './ShowList/ShowList';
-export { default as ListHeader } from './ListHeader/ListHeader';
-export { default as CartIcon } from './Cart/CartIcon';
-export { default as CircleView } from './CircleView/CircleView';
-export { default as CartList } from './Cart/CartList';
