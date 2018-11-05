2906b755db77307ac4f194ba8fbb91cbd6fe5fb9
PanupongDev <5635512110@psu.ac.th>
2018-11-06 00:12:45 +0700

    update service

--- src/class/ServicesAPI/๊UsersService.js
+++ src/class/ServicesAPI/๊UsersService.js
@@ -9,6 +9,45 @@ class UsersService extends BaseService {
         this.ots = UsersOTS;
     }
 
+    async get() {
+        try {
+          const resToken = await this.axios.get(`${this.RootURL}/${this.TokenURL}?token=${this.storage.getToken()}`);
+          this.storage.saveToken(resToken.data.result.token);
+          this.setConfig();
+          const res = await this.axios.get(
+            `${this.RootURL}/${this.domain}`,
+            this.config
+          );
+          return res.data.result;
+        } catch (error) {
+          if(error && error.response && error.response.data && error.response.data.result && error.response.data.result.name === 'TokenExpiredError') {
+            this.storage.removeStorage();
+            window.location.reload();
+            return;
+          }
+          throw error;
+        }
+      }
+      async getById(id) {
+        try {
+          const resToken = await this.axios.get(`${this.RootURL}/${this.TokenURL}?token=${this.storage.getToken()}`);
+          this.storage.saveToken(resToken.data.result.token);
+          this.setConfig();
+          const res = await this.axios.get(
+            `${this.RootURL}/${this.domain}/${id}`,
+            this.config
+          );
+          return res.data.result;
+        } catch (error) {
+          if(error && error.response && error.response.data && error.response.data.result && error.response.data.result.name === 'TokenExpiredError') {
+            this.storage.removeStorage();
+            window.location.reload();
+            return;
+          }
+          throw error;
+        }
+      }
+
     async authentication(data) {
         try {
             const res = await this.axios.post(
